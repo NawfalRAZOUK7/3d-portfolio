4a161975d5f9c0e15398e4d8962317f37c460b5c
Nawfal RAZOUK <nawfalrazouk7@gmail.com>
2024-05-21 11:36:50 +0100

    add a button to download CV

--- src/components/pages/About/About.jsx
+++ src/components/pages/About/About.jsx
@@ -4,6 +4,16 @@ import { MdFacebook } from "react-icons/md";
 import Heading from "../../HelperComp/Heading";
 
 const About = () => {
+
+  const handleDownloadResume = () => {
+    const link = document.createElement('a');
+    link.href = require("../../../assets/my_resume.pdf");
+    link.download = 'Nawfal_Razouk_Resume.pdf';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
     <div className="h-full page-shadow bg-white p-10">
       {/* heading */}
@@ -13,38 +23,37 @@ const About = () => {
       />
 
       {/* Details section  */}
-
       <div className="mt-10 text-slate-500">
         <p>
-        I am Nawfal Razouk, an IT engineering student at Mines Rabat with a strong proficiency in both front-end and back-end development, including technologies like ReactJS, PHP, and Java. I have created various projects, such as portfolio websites and web applications, showcasing my skills in HTML, CSS, JavaScript, and cloud services.
-        </p>{" "}
+          I am Nawfal Razouk, an IT engineering student at Mines Rabat with a strong proficiency in both front-end and back-end development, including technologies like ReactJS, PHP, and Java. I have created various projects, such as portfolio websites and web applications, showcasing my skills in HTML, CSS, JavaScript, and cloud services.
+        </p>
         <br />
         <p>
-        Additionally, I have leadership experience as the founder of the Mines IT Club and am fluent in Arabic, French, and English, making me a versatile and valuable tech professional.
+          Additionally, I have leadership experience as the founder of the Mines IT Club and am fluent in Arabic, French, and English, making me a versatile and valuable tech professional.
         </p>
         {/* Social Links */}
         <div className="flex space-x-4 mt-10">
-  <a href="https://www.facebook.com/profile.php?id=61554968127571&mibextid=LQQJ4d" target="_blank" rel="noopener noreferrer">
-    <MdFacebook className="social-btn" />
-  </a>
-  <a href="https://www.instagram.com/nawfalrazouk7?igsh=MWM0cGNxdHV3MzB3bQ%3D%3D&utm_source=qr" target="_blank" rel="noopener noreferrer">
-    <AiFillInstagram className="social-btn" />
-  </a>
-  <a href="https://github.com/NawfalRAZOUK7" target="_blank" rel="noopener noreferrer">
-    <AiFillGithub className="social-btn" />
-  </a>
-  <a href="https://www.linkedin.com/in/nawfal-razouk?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" target="_blank" rel="noopener noreferrer">
-    <AiFillLinkedin className="social-btn" />
-  </a>
-</div>
+          <a href="https://www.facebook.com/profile.php?id=61554968127571&mibextid=LQQJ4d" target="_blank" rel="noopener noreferrer">
+            <MdFacebook className="social-btn" />
+          </a>
+          <a href="https://www.instagram.com/nawfalrazouk7?igsh=MWM0cGNxdHV3MzB3bQ%3D%3D&utm_source=qr" target="_blank" rel="noopener noreferrer">
+            <AiFillInstagram className="social-btn" />
+          </a>
+          <a href="https://github.com/NawfalRAZOUK7" target="_blank" rel="noopener noreferrer">
+            <AiFillGithub className="social-btn" />
+          </a>
+          <a href="https://www.linkedin.com/in/nawfal-razouk?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" target="_blank" rel="noopener noreferrer">
+            <AiFillLinkedin className="social-btn" />
+          </a>
+        </div>
 
         {/* Button Links */}
         <div className="flex space-x-4 mt-4">
-          <a href="../../../assets/my_resume.pdf" className="primary-btn">
+          <button onClick={handleDownloadResume} className="primary-btn">
             Download Resume
-          </a>
+          </button>
           <a href="https://chatbot-razouk.vercel.app/" className="outline-btn">
-            Chat with my chatbot ! 
+            Chat with my chatbot !
           </a>
         </div>
       </div>
